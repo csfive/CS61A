49d8795196b8bac4951d3960a88457fb0467e54c
mancuoj <mancuoj@gmail.com>
2024-04-27 23:35:05 +0800

    feat: disc1

---
+++ discussions/disc01/fizzbuzz.py
@@ -0,0 +1,41 @@
+def fizzbuzz(n):
+    """
+    >>> result = fizzbuzz(16)
+    1
+    2
+    fizz
+    4
+    buzz
+    fizz
+    7
+    8
+    fizz
+    buzz
+    11
+    fizz
+    13
+    14
+    fizzbuzz
+    16
+    >>> print(result)
+    None
+    """
+    for i in range(1, n + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            print("fizzbuzz")
+        elif i % 3 == 0:
+            print("fizz")
+        elif i % 5 == 0:
+            print("buzz")
+        else:
+            print(i)
+
+
+def fizzbuzz2(n):
+    for i in range(1, n + 1):
+        ans = ""
+        if i % 3 == 0:
+            ans += "fizz"
+        if i % 5 == 0:
+            ans += "buzz"
+        print(ans or i)
---
+++ discussions/disc01/is_prime.py
@@ -0,0 +1,22 @@
+def is_prime(n):
+    """
+    >>> is_prime(10)
+    False
+    >>> is_prime(7)
+    True
+    >>> is_prime(1) # one is not a prime number!!
+    False
+    >>> is_prime(2)
+    True
+    """
+    if n < 2:
+        return False
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+    i = 3
+    while i * i < n:
+        if n % i == 0:
+            return False
+    return True
