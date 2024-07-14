23fc36de63ffd72cb8f4101d71d3dbfc6ec651b1
mancuoj <mancuoj@gmail.com>
2024-07-14 16:14:52 +0800

    chore: update

--- labs/lab00/lab00.py
+++
@@ -1,8 +0,0 @@
-def twenty_twenty_four():
-    """Come up with the most creative expression that evaluates to 2024
-    using only numbers and the +, *, and - operators.
-
-    >>> twenty_twenty_four()
-    2024
-    """
-    return 2001 + 23
