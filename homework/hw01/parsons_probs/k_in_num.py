3990eced821a5b5a3ce95b10464849476e2eff4e
mancuoj <mancuoj@gmail.com>
2024-07-15 22:08:55 +0800

    chore: update

---
+++ homework/hw01/parsons_probs/k_in_num.py
@@ -0,0 +1,22 @@
+def k_in_num(k, num):
+    """
+    Complete k_in_num, a function which returns True if num has the digit k and
+    returns False if num does not have the digit k. 0 is considered to have no
+    digits.
+
+    >>> k_in_num(3, 123) # .Case 1
+    True
+    >>> k_in_num(2, 123) # .Case 2
+    True
+    >>> k_in_num(5, 123) # .Case 3
+    False
+    >>> k_in_num(0, 0) # .Case 4
+    False
+    """
+    while num:
+        if k == num % 10:
+            return True
+        num = num // 10
+    return False
+
+
