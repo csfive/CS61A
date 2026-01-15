c61e3b13c1e4d9c55cb43491dc24d1e844a78b91
mancuoj <mancuoj@gmail.com>
2026-01-15 21:58:11 +0800

    lab00

---
+++ lab00/lab00.py
@@ -0,0 +1,8 @@
+def twenty_twenty_five():
+    """Come up with the most creative expression that evaluates to 2025
+    using only numbers and the +, *, and - operators (or ** and % if you'd like).
+
+    >>> twenty_twenty_five()
+    2025
+    """
+    return 2001 + 24
---
+++ notes.md
@@ -0,0 +1,17 @@
+# Notes
+
+```sh
+wget https://cs61a.org/lab/lab00/lab00.zip
+unzip lab00.zip
+```
+
+```sh
+# create a virtual environment
+uv venv
+```
+
+```sh
+python3 lab00.py
+python3 -i lab00.py  # call functions you defined
+python3 -m doctest lab00.py
+```
