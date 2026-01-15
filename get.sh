faafa9636f759b7e7faaa4c8e3aa11f572bf8c0a
mancuoj <mancuoj@gmail.com>
2026-01-15 21:58:11 +0800

    lab00

---
+++ get.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+    echo "Usage: $0 <assignment> (e.g. ./get.sh lab00)"
+    exit 1
+fi
+
+NAME=$1
+TYPE=${NAME//[0-9]/}
+[ "$TYPE" = "$NAME" ] && TYPE="proj"
+URL="https://cs61a.org/${TYPE}/${NAME}/${NAME}.zip"
+
+echo "🎯 Target: $URL" && \
+wget -q "$URL" && \
+unzip -q "${NAME}.zip" && \
+echo "✨ $NAME is ready!" && \
+cd "$NAME" || exit 1
