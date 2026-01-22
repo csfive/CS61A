beb2fc9ab5e02a68ad11b0ffb47f3d6232093e2a
clcs <143490477+clcs@users.noreply.github.com>
2026-01-22 05:55:48 +0000

    init

---
+++ lab02/parsons_probs/digit_index_factory.py
@@ -0,0 +1,17 @@
+
+def digit_index_factory(num, k):
+    """
+    Returns a function that takes no arguments, and outputs the offset
+    between k and the rightmost digit of num. If k is not in num, then
+    the returned function returns -1. Note that 0 is considered to
+    contain no digits (not even 0).
+    >>> digit_index_factory(34567, 4)() # .Case 1
+    3
+    >>> digit_index_factory(30001, 0)() # .Case 2
+    1
+    >>> digit_index_factory(999, 1)() # .Case 3
+    -1
+    >>> digit_index_factory(1234, 0)() # .Case 4
+    -1
+    """
+    "*** YOUR CODE HERE ***"
---
+++ lab02/parsons_probs/hop.py
@@ -0,0 +1,11 @@
+def hop():
+    """
+    Calling hop returns a curried version of the function f(x, y) = y.
+    >>> hop()(3)(2) # .Case 1
+    2
+    >>> hop()(3)(7) # .Case 2
+    7
+    >>> hop()(4)(7) # .Case 3
+    7
+    """
+    "*** YOUR CODE HERE ***"
