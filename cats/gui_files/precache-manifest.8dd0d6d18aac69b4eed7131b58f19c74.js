08e0fece4847ba0dff446d8b09bf7b375e629e78
clcs <143490477+clcs@users.noreply.github.com>
2026-01-30 07:27:26 +0000

    cats gui

---
+++ cats/gui_files/precache-manifest.8dd0d6d18aac69b4eed7131b58f19c74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6982e04a08fdf58534f6ae7a288e3140",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e663642e071350a7fde4",
+    "url": "/static/css/2.df9ef6f4.chunk.css"
+  },
+  {
+    "revision": "483fa15d2251b891276e",
+    "url": "/static/css/main.dbfcd040.chunk.css"
+  },
+  {
+    "revision": "e663642e071350a7fde4",
+    "url": "/static/js/2.ecdaf578.chunk.js"
+  },
+  {
+    "revision": "483fa15d2251b891276e",
+    "url": "/static/js/main.de7e9ae2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
