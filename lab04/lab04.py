c768cbb4bc122d3716ba726fe87c2b1458eaa1ac
clcs <143490477+clcs@users.noreply.github.com>
2026-01-30 07:00:32 +0000

    lab04 without parsons

--- lab04/lab04.py
+++ lab04/lab04.py
@@ -19,7 +19,9 @@ def summation(n, term):
     True
     """
     assert n >= 1
-    "*** YOUR CODE HERE ***"
+    if n == 1:
+        return term(n)
+    return term(n) + summation(n - 1, term)
 
 
 def paths(m, n):
@@ -35,7 +37,9 @@ def paths(m, n):
     >>> paths(1, 157)
     1
     """
-    "*** YOUR CODE HERE ***"
+    if m == 1 or n == 1:
+        return 1
+    return paths(m - 1, n) + paths(m, n - 1)
 
 
 def pascal(row, column):
@@ -50,4 +54,8 @@ def pascal(row, column):
     >>> pascal(4, 2)     # Row 4 (1 4 6 4 1), Column 2
     6
     """
-    "*** YOUR CODE HERE ***"
+    if column < 0 or row - column < 0:
+        return 0
+    if column == 0 or row == column:
+        return 1
+    return pascal(row - 1, column) + pascal(row - 1, column - 1)
