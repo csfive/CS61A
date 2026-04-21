2aa09143a5a748cf1b1afed2801208d343c56a0c
clcs <143490477+clcs@users.noreply.github.com>
2026-04-21 10:29:20 +0000

    init hw04

---
+++ hw04/parsons_probs/remove_odd_indices.py
@@ -0,0 +1,15 @@
+def remove_odd_indices(lst, odd):
+    """ 
+    Remove elements of lst that have odd indices.
+    >>> s = [1, 2, 3, 4]
+    >>> t = remove_odd_indices(s, True)
+    >>> s
+    [1, 2, 3, 4]
+    >>> t
+    [1, 3]
+    >>> l = [5, 6, 7, 8]
+    >>> m = remove_odd_indices(l, False)
+    >>> m
+    [6, 8]
+    """
+    "*** YOUR CODE HERE ***"
