b086c45e030fa3d9b34bd15834e097d04522b244
clcs <143490477+clcs@users.noreply.github.com>
2026-05-25 07:36:07 +0000

    init

---
+++ hw05/parsons_probs/flatten_link.py
@@ -0,0 +1,36 @@
+def flatten_link(lnk):
+    """Takes a linked list and returns a flattened Python list with the same elements.
+
+    >>> link = Link(1, Link(2, Link(3, Link(4))))
+    >>> flatten_link(link)
+    [1, 2, 3, 4]
+    >>> flatten_link(Link.empty)
+    []
+    >>> deep_link = Link(Link(1, Link(2, Link(3, Link(4)))), Link(Link(5), Link(6)))
+    >>> flatten_link(deep_link)
+    [1, 2, 3, 4, 5, 6]
+    """
+    "*** YOUR CODE HERE ***"
+
+
+class Link:
+    empty = ()
+
+    def __init__(self, first, rest=empty):
+        assert rest is Link.empty or isinstance(rest, Link)
+        self.first = first
+        self.rest = rest
+
+    def __repr__(self):
+        if self.rest is not Link.empty:
+            rest_repr = ', ' + repr(self.rest)
+        else:
+            rest_repr = ''
+        return 'Link(' + repr(self.first) + rest_repr + ')'
+
+    def __str__(self):
+        string = '<'
+        while self.rest is not Link.empty:
+            string += str(self.first) + ' '
+            self = self.rest
+        return string + str(self.first) + '>'
