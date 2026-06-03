638c321117ca1a777606b83a9dfa11de12bd370e
clcs <143490477+clcs@users.noreply.github.com>
2026-06-03 08:48:18 +0000

    lab10

--- lab10/lab10.scm
+++ lab10/lab10.scm
@@ -1,9 +1,25 @@
-(define (over-or-under num1 num2) 'YOUR-CODE-HERE)
+(define (over-or-under num1 num2)
+    (cond
+        ((> num1 num2) 1)
+        ((< num1 num2) -1)
+        (else 0)
+    )
+)
 
-(define (make-adder num) 'YOUR-CODE-HERE)
+(define (make-adder num) 
+    (lambda (inc) (+ num inc))
+)
 
-(define (composed f g) 'YOUR-CODE-HERE)
+(define (composed f g) 
+    (lambda (x) (f (g x)))
+)
 
 (define (square n) (* n n))
 
-(define (pow base exp) 'YOUR-CODE-HERE)
+(define (pow base exp)
+    (cond
+        ((= exp 0) 1)
+        ((even? exp) (square (pow base (/ exp 2))))
+        (else (* base (pow base (- exp 1))))
+    )
+)
--- lab10/tests/pow.py
+++ lab10/tests/pow.py
@@ -7,41 +7,37 @@ test = {
         {
           'code': r"""
           scm> (pow 2 5)
-          0dadfcf7bdb0ebdf4252ae238ea5211f
-          # locked
+          32
           """,
           'hidden': False,
-          'locked': True,
+          'locked': False,
           'multiline': False
         },
         {
           'code': r"""
           scm> (pow 10 3)
-          b31bbc50377bdf9716f8e53472ba4628
-          # locked
+          1000
           """,
           'hidden': False,
-          'locked': True,
+          'locked': False,
           'multiline': False
         },
         {
           'code': r"""
           scm> (pow 3 3)
-          39ece6e316893e79c1d4850fc45ac69b
-          # locked
+          27
           """,
           'hidden': False,
-          'locked': True,
+          'locked': False,
           'multiline': False
         },
         {
           'code': r"""
           scm> (pow 1 100000000000000) ; make sure this doesn't run forever!
-          5d57f236bfa316cde9f9cd563993dae4
-          # locked
+          1
           """,
           'hidden': False,
-          'locked': True,
+          'locked': False,
           'multiline': False
         }
       ],
