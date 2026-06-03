1af4dd83d9775dfc825f21a472122a6999380071
clcs <143490477+clcs@users.noreply.github.com>
2026-06-03 08:48:24 +0000

    reformat

--- lab10/lab10.scm
+++ lab10/lab10.scm
@@ -1,25 +1,18 @@
 (define (over-or-under num1 num2)
-    (cond
-        ((> num1 num2) 1)
-        ((< num1 num2) -1)
-        (else 0)
-    )
-)
+  (cond 
+    ((> num1 num2) 1)
+    ((< num1 num2) -1)
+    (else          0)))
 
-(define (make-adder num) 
-    (lambda (inc) (+ num inc))
-)
+(define (make-adder num)
+  (lambda (inc) (+ num inc)))
 
-(define (composed f g) 
-    (lambda (x) (f (g x)))
-)
+(define (composed f g) (lambda (x) (f (g x))))
 
 (define (square n) (* n n))
 
 (define (pow base exp)
-    (cond
-        ((= exp 0) 1)
-        ((even? exp) (square (pow base (/ exp 2))))
-        (else (* base (pow base (- exp 1))))
-    )
-)
+  (cond 
+    ((= exp 0)   1)
+    ((even? exp) (square (pow base (/ exp 2))))
+    (else        (* base (pow base (- exp 1))))))
