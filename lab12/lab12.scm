7888b7436787edb5fb812fa71c43fa006a37e7ed
clcs <143490477+clcs@users.noreply.github.com>
2026-06-04 06:50:52 +0000

    lab12

--- lab12/lab12.scm
+++ lab12/lab12.scm
@@ -8,10 +8,19 @@
 
 (define (get-lon city) (car (cdr (cdr city))))
 
-(define (distance city-a city-b) 'YOUR-CODE-HERE)
+(define (distance city-a city-b)
+  (define lat-a (get-lat city-a))
+  (define lon-a (get-lon city-a))
+  (define lat-b (get-lat city-b))
+  (define lon-b (get-lon city-b))
+  (sqrt (+ (expt (- lat-a lat-b) 2)
+           (expt (- lon-a lon-b) 2))))
 
 (define (closer-city lat lon city-a city-b)
-  'YOUR-CODE-HERE)
+  (if (< (distance city-a (make-city 'temp lat lon))
+         (distance city-b (make-city 'temp lat lon)))
+      (get-name city-a)
+      (get-name city-b)))
 
 ; Teacher and Student Abstractions
 (define (student-create name classes)
@@ -20,26 +29,32 @@
 (define (teacher-create name class students)
   (cons name (cons class students)))
 
-(define (student-get-name student)
-  'YOUR-CODE-HERE)
+(define (student-get-name student) (car student))
 
 (define (student-get-classes student)
-  'YOUR-CODE-HERE)
+  (cdr student))
 
-(define (teacher-get-name teacher)
-  'YOUR-CODE-HERE)
+(define (teacher-get-name teacher) (car teacher))
 
 (define (teacher-get-class teacher)
-  'YOUR-CODE-HERE)
+  (car (cdr teacher)))
 
 (define (teacher-get-students teacher)
-  'YOUR-CODE-HERE)
+  (cdr (cdr teacher)))
 
 (define (student-attend-class student class)
-  'YOUR-CODE-HERE)
+  (student-create (student-get-name student)
+                  (cons class (student-get-classes student))))
 
 (define (teacher-hold-class teacher)
-  'YOUR-CODE-HERE)
+  (define new-students
+          (map (lambda (student)
+                 (student-attend-class student
+                                       (teacher-get-class teacher)))
+               (teacher-get-students teacher)))
+  (teacher-create (teacher-get-name teacher)
+                  (teacher-get-class teacher)
+                  new-students))
 
 ; Rational Abstraction
 ; Helpers
--- lab12/tests/abstractions.py
+++ lab12/tests/abstractions.py
@@ -7,94 +7,70 @@ test = {
         {
           'code': r"""
           scm> (define x (rational 2 5))
-          ecf2b304041c91c68610920edf6214eb
-          # locked
+          x
           scm> (numer x)
-          b764c5386326cd8ae89e4efe0c923e84
-          # locked
+          2
           scm> (denom x)
-          3b2d9316bebaa3b3f233e455d255a9e1
-          # locked
+          5
           scm> (define y (rational 1 4))
-          87e09802e0243f4d9aa26908b733336c
-          # locked
+          y
           scm> (define z1 (add-rational x y))
-          32eb0b0bab5709764f7684e503498151
-          # locked
+          z1
           scm> (numer z1)
-          d7c7eb47da54a13f980493ac7d8b9b65
-          # locked
+          13
           scm> (denom z1)
-          f9ff38eeedff271ae8c1da1dd024b2ec
-          # locked
+          20
           scm> (define z2 (mul-rational x y))
-          3bc71d780e6b21e0abd53fbffcb24bd2
-          # locked
+          z2
           scm> (numer z2)
-          f313ff1ce5644f8b5465a3f45b4f19a4
-          # locked
+          1
           scm> (denom z2)
-          ff2443dd373fbd084719804738d09b80
-          # locked
+          10
           """,
           'hidden': False,
-          'locked': True,
+          'locked': False,
           'multiline': False
         },
         {
           'code': r"""
           scm> (define t (tree 1 (list (tree 2 nil))))
-          39acca2d57ab183dd99453a5334d67d5
-          # locked
+          t
           scm> (label t)
-          f313ff1ce5644f8b5465a3f45b4f19a4
-          # locked
+          1
           scm> (length (branches t))
-          f313ff1ce5644f8b5465a3f45b4f19a4
-          # locked
+          1
           scm> (define child (car (branches t)))
-          1deb9adcb2e2af283609ea135533bf8b
-          # locked
+          child
           scm> (label child)
-          b764c5386326cd8ae89e4efe0c923e84
-          # locked
+          2
           scm> (is-leaf child)
-          309b33224b9a47888ae45fc6e19fa6e6
-          # locked
+          #t
           scm> (branches child)
-          afed03a4bad4b019a15373c410ea3792
-          # locked
+          ()
           """,
           'hidden': False,
-          'locked': True,
+          'locked': False,
           'multiline': False
         },
         {
           'code': r"""
           scm> (define b1 (tree 5 (list (tree 6 nil) (tree 7 nil)) )) 
-          8b3cdc93a1a6616cbeb588569b2d5c80
-          # locked
+          b1
           scm> (map is-leaf (branches b1))  ; draw the tree if you get stuck!
-          cf2835b345e001ebb561ae95c23d30e3
-          # locked
+          (#t #t)
           scm> (define b2 (tree 8 (list (tree 9 (list (tree 10 nil)) )) )) 
-          efa3f8d62df45591d10f7f251966583f
-          # locked
+          b2
           scm> (map is-leaf (branches b2))  ; draw the tree if you get stuck!
-          8297350b7fdb18a1dbdb44e47abeadde
-          # locked
+          (#f)
           scm> (define t (tree 11 (list b1 b2)))
-          39acca2d57ab183dd99453a5334d67d5
-          # locked
+          t
           scm> (label t)
-          28ee5399d75ef3bec6592a11a761594f
-          # locked
+          11
           scm> (map (lambda (b) (label b)) (branches t))
-          5e6e197c7088e8b03fe7bdd26aa1d738
-          # locked
+          (5 8)
           """,
           'hidden': False,
-          'locked': True,
+          'locked': False,
           'multiline': False
         }
       ],
